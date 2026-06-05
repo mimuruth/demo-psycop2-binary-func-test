0d622adfdefc71c00b67088e3c933205e82e36a5
Michael Muruthi <mimuruth@microsoft.com>
2026-06-05 13:58:20 -0500

    first commit

---
+++ function_app.py
@@ -0,0 +1,28 @@
+import json
+import logging
+ 
+import azure.functions as func
+import psycopg2
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="test_psycop2")
+def test_psycop2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
